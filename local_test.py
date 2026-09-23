9079be1d88ec3850f690c7d2a19c83571fb03be0
Thavaisya Ananth <tananth3@gatech.edu>
2026-09-23 13:50:53 -0400

    Change URL and update screenshot/source file handling
    Updated URL for NE Atl and modified ss and source file names for the new site.

--- local_test.py
+++ local_test.py
@@ -12,25 +12,16 @@ options.add_argument("--no-sandbox")
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
-url = "https://webapps.jacksonemc.com/nisc/maps/MemberOutageMap/"
-# driver.get(url)
-# time.sleep(10)
+url = "https://ebill.grayson-collin.coop/maps/ext/OutageWebMap/"
+driver.get(url)
+time.sleep(10)
 
-# print("Page title:", driver.title)
-# driver.save_screenshot("new_site_screenshot.png")
+print("Page title:", driver.title)
+driver.save_screenshot("grayson_collin_screenshot.png")
 
-# with open("new_site_source.html", "w", encoding="utf-8") as f:
-#     f.write(driver.page_source)
+with open("grayson_collin_source.html", "w", encoding="utf-8") as f:
+    f.write(driver.page_source)
 
-# print("Saved ss and page source.")
-
-# time.sleep(20)
-# driver.quit()
-data = scraper(url, driver)
-
-driver.close()
+print("Saved screenshot and page source.")
+time.sleep(15)
 driver.quit()
-
-for key, df in data.items():
-    print(f"--- {key} ---")
-    print(df.head())
